e8a747bcaa801fd75499097556bda1de253f97c4
AlexL70 <alexander.levinson.70@gmail.com>
2016-06-23 18:20:35 +0300

    Merge Sort algorihm has been implemented

---
+++ AlgorithmTest/MergeSortTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using Algorithms.Sorting;
+
+namespace AlgorithmTest
+{
+    [TestFixture, Category(nameof(Algorithms.Sorting))]
+    public class MergeSortTest
+    {
+        [Test]
+        public void TestMS_1()
+        {
+            int[] arr = { 4, 7, 12, 5, 44, 3, 9, 1, 32, 15 };
+            MergeSort<int>.Sort(arr);
+            int[] sorted = { 1, 3, 4, 5, 7, 9, 12, 15, 32, 44 };
+            Assert.AreEqual(sorted, arr);
+        }
+
+        [Test]
+        public void TestMS_2()
+        {
+            int[] arr = { 3, 5, 1, 4, 2 };
+            MergeSort<int>.Sort(arr);
+            int[] sorted = { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(sorted, arr);
+        }
+
+        [Test]
+        public void TestMS_Inv()
+        {
+            int[] arr = { 1, 3, 5, 2, 4, 6 };
+            int inversions = MergeSort<int>.Sort(arr);
+            int[] sorted = { 1, 2, 3, 4, 5, 6 };
+            Assert.AreEqual(sorted, arr);
+            Assert.AreEqual(3, inversions);
+        }
+
+
+    }
+}
---
+++ Algorithms/MergeSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Algorithms.Sorting
+{
+    public static class MergeSort<T>
+        where T : IComparable<T>
+    {
+        public static int Sort(T[] arr)
+        {
+            int Inversions;
+            T[] copy1 = new T[arr.Length];
+            Inversions = Sort(arr, copy1, 0, arr.Length - 1);
+            copy1.CopyTo(arr, 0);
+            return Inversions;
+        }
+
+        private static int Sort(T[] src, T[] dest, int left, int right, int level = 1)
+        {
+            if (left < right)
+            {
+                var middle = left + (right - left) / 2;
+                var inv = Sort(dest, src, left, middle, level + 1);
+                inv += Sort(dest, src, middle + 1, right, level + 1);
+                inv += MergeAndCountInversions(src, dest, left, middle, right);
+                return inv;
+            }
+            else
+            {
+                if (level % 2 == 1)
+                    dest[left] = src[left];
+                return 0;
+            }
+        }
+
+        private static int MergeAndCountInversions(T[] src, T[] dest, int left, int middle, int right)
+        {
+            int inv = 0;
+            int i = left;
+            int j = middle + 1;
+            for (int x = left; x <= right; x++)
+            {
+                if (j > right || (i <= middle && src[i].CompareTo(src[j]) <= 0))
+                {
+                    dest[x] = src[i];
+                    i++;
+                }
+                else
+                {
+                    dest[x] = src[j];
+                    j++;
+                    inv += middle - i + 1;
+                }
+            }
+            return inv;
+        }
+    }
+}
