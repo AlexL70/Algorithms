14c6c198d30ceb7e984089ebd5a932720db026b7
AlexL70 <alexander.levinson.70@gmail.com>
2016-07-25 20:25:10 +0300

    Dijkstra algorithm; couple more test-cases

--- AlgorithmTest/DirectedGraphTest.cs
+++ AlgorithmTest/DirectedGraphTest.cs
@@ -3,7 +3,6 @@ using Algorithms.Graphs;
 using System;
 using System.Linq;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace AlgorithmTest
 {
@@ -156,6 +155,7 @@ namespace AlgorithmTest
     public class DijkstraTestData
     {
         #region Dijkstra hardcoded test data
+        #region t1
         private static Tuple<int, int, int>[] t1 = new Tuple<int, int, int>[]
         {
             Tuple.Create( 1, 2, 1 ),
@@ -172,7 +172,8 @@ namespace AlgorithmTest
             Tuple.Create(3, 3),
             Tuple.Create(4, 6)
         };
-
+        #endregion
+        #region t2
         private static Tuple<int, int, int>[] t2 = new Tuple<int, int, int>[]
         {
             Tuple.Create(1, 2, 2),
@@ -200,12 +201,108 @@ namespace AlgorithmTest
             Tuple.Create(7, 6),
         };
         #endregion
+        #region t3
+        private static Tuple<int, int, int>[] t3 = new Tuple<int, int, int>[]
+        {
+            Tuple.Create( 1, 2, 1),
+            Tuple.Create( 1, 5, 3),
+            Tuple.Create( 1, 8, 3),
+            Tuple.Create( 2, 1, 1),
+            Tuple.Create( 2, 3, 4),
+            Tuple.Create( 2, 7, 2),
+            Tuple.Create( 2, 8, 1),
+            Tuple.Create( 3, 2, 4),
+            Tuple.Create( 3, 4, 1),
+            Tuple.Create( 3, 5, 2),
+            Tuple.Create( 3, 7, 1),
+            Tuple.Create( 3, 8, 3),
+            Tuple.Create( 4, 3, 1),
+            Tuple.Create( 5, 1, 3),
+            Tuple.Create( 5, 3, 2),
+            Tuple.Create( 5, 6, 1),
+            Tuple.Create( 5, 7, 1),
+            Tuple.Create( 6, 5, 1),
+            Tuple.Create( 7, 2, 2),
+            Tuple.Create( 7, 3, 1),
+            Tuple.Create( 7, 5, 1),
+            Tuple.Create( 7, 8, 2),
+            Tuple.Create( 8, 1, 3),
+            Tuple.Create( 8, 2, 1),
+            Tuple.Create( 8, 3, 3),
+            Tuple.Create( 8, 7, 2),
+            Tuple.Create( 8, 9, 1),
+            Tuple.Create( 9, 8, 1),
+            Tuple.Create( 9, 10, 3),
+            Tuple.Create( 9, 11, 2),
+            Tuple.Create( 10, 9, 3),
+            Tuple.Create( 10, 11, 3),
+            Tuple.Create( 11, 9, 2),
+            Tuple.Create( 11, 10, 3)
+        };
+
+        private static Tuple<int, int>[] t3_paths = new Tuple<int, int>[]
+        {
+            Tuple.Create( 1, 0),
+            Tuple.Create( 2, 1),
+            Tuple.Create( 3, 4),
+            Tuple.Create( 4, 5),
+            Tuple.Create( 5, 3),
+            Tuple.Create( 6, 4),
+            Tuple.Create( 7, 3),
+            Tuple.Create( 8, 2),
+            Tuple.Create( 9, 3),
+            Tuple.Create( 10, 6),
+            Tuple.Create(11, 5)
+        };
+        #endregion
+        #region t4
+        private static Tuple<int, int, int>[] t4 = new Tuple<int, int, int>[]
+        {
+            Tuple.Create( 1, 2, 1),
+            Tuple.Create( 1, 8, 2),
+            Tuple.Create( 2, 1, 1),
+            Tuple.Create( 2, 3, 1),
+            Tuple.Create( 3, 2, 1),
+            Tuple.Create( 3, 4, 1),
+            Tuple.Create( 4, 3, 1),
+            Tuple.Create( 4, 5, 1),
+            Tuple.Create( 5, 4, 1),
+            Tuple.Create( 5, 6, 1),
+            Tuple.Create( 6, 5, 1),
+            Tuple.Create( 6, 7, 1),
+            Tuple.Create( 7, 6, 1),
+            Tuple.Create( 7, 8, 1),
+            Tuple.Create( 8, 7, 1),
+            Tuple.Create( 8, 1, 2)
+        };
+        private static Tuple<int, int>[] t4_paths = new Tuple<int, int>[]
+        {
+            Tuple.Create( 1, 0), // []
+            Tuple.Create( 2, 1), // [2]
+            Tuple.Create( 3, 2), // [2, 3]
+            Tuple.Create( 4, 3), // [2, 3, 4]
+            Tuple.Create( 5, 4), // [2, 3, 4, 5]
+            Tuple.Create( 6, 4), // [8, 7, 6]
+            Tuple.Create( 7, 3), // [8, 7]
+            Tuple.Create( 8, 2)  // [8]
+        };
+        #endregion
+        #endregion
+
+        private static TestCaseData prepareTestData(Tuple<int, int, int>[] input, Tuple<int, int>[] output, string name_ext)
+        {
+            var data = new TestCaseData(input, output);
+            data.TestName = $"{nameof(DijksgraGraphTest.DijkstraShortestPath_Test)}_{name_ext}";
+            return data;
+        }
         public static IEnumerable TestCases
         {
             get
             {
-                yield return new TestCaseData( t1, t1_paths);
-                yield return new TestCaseData(t2, t2_paths);
+                yield return prepareTestData(t1, t1_paths, nameof(t1));
+                yield return prepareTestData(t2, t2_paths, nameof(t2));
+                yield return prepareTestData(t3, t3_paths, nameof(t3));
+                yield return prepareTestData(t4, t4_paths, nameof(t4));
             }
         }
     }
